2e193d39e103aa372db1d9fd2df34dd6bc951689
shacharracheli <r0583237001@gmail.com>
2025-07-31 17:05:05 +0300

    rtl try

--- Tene-react/src/App.tsx
+++ Tene-react/src/App.tsx
@@ -1,22 +1,17 @@
 import { RouterProvider } from "react-router"
 import { router } from "./router"
 import Footer from "./footer"
-import MovingDetailsForm from "./components/forms/movingDetailsForm";
 import store from "./store/store";
 import { Provider } from 'react-redux';
 
 function App() {
-
+  
   return (
     <>
-      <Provider store={store}>
-      <RouterProvider router={router} />
-      {/* <MovingDetailsForm/>  */}
-      <Footer/>
-      </Provider> 
-    {/* 
-     <Footer/> 
-  */}
+        <Provider store={store}>
+          <RouterProvider router={router} />
+          <Footer />
+        </Provider>
     </>
   )
 }
